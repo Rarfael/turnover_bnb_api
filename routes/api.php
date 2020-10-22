f53b545acb22401b8c9a2426bf700f2435a8e584
Rarfael <rarfaelgoodoy@gmail.com>
2020-10-22 11:38:33 -0300

    Deleted User model, migrations, validations and authentications, since will not be used

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,3 @@ use Illuminate\Http\Request;
 |
 */
 Route::get('/', fn() => 'API up');
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
