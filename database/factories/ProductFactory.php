d397fa273c3c98073fe23a9d79dbfbb08a475d62
Rarfael <rarfaelgoodoy@gmail.com>
2020-10-23 17:14:07 -0300

    Product migration, controller and DatabaseSeeder created

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -5,11 +5,13 @@
 use Faker\Generator as Faker;
 
 $factory->define(\App\Product::class, function (Faker $faker) {
+    $now = \Carbon\Carbon::now();
     return [
-        'id' => $faker->numberBetween(1, 500),
         'name' => $faker->word,
         'description' => $faker->sentence,
         'price' => $faker->numberBetween(1, 123456),
         'currency' => $faker->randomElement(['BRL', 'USD']),
+        'created_at' => $now,
+        'updated_at' => $now,
     ];
 });
